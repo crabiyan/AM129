20bb6c7242e5f803c5d316a337df0d997d4f61eb
Maya Apotheker <mapothek@ucsc.edu>
2019-10-22 16:15:57 -0700

    some progress

---
+++ hw3/trapezoidIntegral.f90
@@ -0,0 +1,27 @@
+
+program trapezoidIntegral
+        implicit none
+        real,dimension(25) :: xVals
+        real accum
+        
+end program trapezoidIntegral
+
+function trapezoidFunc(xVals, accum,a,b)
+        real,dimension(25), intent(inout) :: xVals
+        real deltaX
+        accum=0
+        xVals(1)=0
+
+        a=0
+        b=1
+
+        deltaX=(b-a)/25 
+        do i=2, 25
+            xVals(i)=i*deltaX    
+        end do
+
+
+
+
+
+end function trapezoidFunc
