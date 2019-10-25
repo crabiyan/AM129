e70d5c3a2ed759dae774c6a1a30ee62a51cd88a3
Maya Apotheker <mapothek@ucsc.edu>
2019-10-25 11:50:11 -0700

    subroutine stuff

--- hw3/trapezoidIntegral.f90
+++ hw3/trapezoidIntegral.f90
@@ -1,13 +1,14 @@
 
 
 program trapezoidIntegral
-use trapezoidApprox, only: trapezoidFunc,f
+use trapezoidApprox, only: trapezoidFunc,f,sub1 
         implicit none
-        real:: n,test
+        real:: n,test, test2
 !        real(kind=8) :: trapezoidFunc
  !       real(kind=8) :: f,x
         print *, 'hi'
-        test= trapezoidFunc(50d0,0d0,1d0)
+        test= trapezoidFunc(400d0,0d0,1d0)
+		call sub1(400d0,0d0,1d0, test2)
         print *, test
 end program trapezoidIntegral
 
--- hw3/trapezoid_mod.f90
+++ hw3/trapezoid_mod.f90
@@ -1,7 +1,7 @@
 
 module  trapezoidApprox
 implicit none
-real(kind=8), parameter:: n = 50
+real(kind=8), parameter:: n = 400, test2=0
 !real(kind=8) ::trapezoidFunc
 !real(kind=8) ::f
 
@@ -39,11 +39,37 @@ function trapezoidFunc(n,a,b) result(aggregate)
 
 end function trapezoidFunc
 
+subroutine sub1(n,a,b, test2)
+        integer(kind=8) :: i
+        real(kind=8) :: x
+        real(kind=8), intent(in):: n
+        real(kind=8), intent(in):: a
+        real(kind=8), intent(in):: b
+        real(kind=8):: deltaX
+        real(kind=8):: firstLast
+        real(kind=8):: middle
+        real(kind=8):: aggregate
+		real(kind=8):: test2
+		test2=0
+        print *, 'hi2'
+        deltaX=(1)/(n)
+        firstLast= deltaX*(f(a)+f(b))
+        do i=2, n-1
+                print *, i*deltaX
+                middle=middle+(f(i*deltaX)*deltaX)
+!                print *, middle
+        end do
+
+                test2=firstLast+middle
+				print *, aggregate
+end subroutine
 
 real(kind=8) function f(x)
 real(kind=8), intent(in):: x
 f=x**2+1
 end function
+
+
 
 
 end module trapezoidApprox
