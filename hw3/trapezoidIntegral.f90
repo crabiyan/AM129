3f48645885efe068159cfe7fee0f25a8cd41f990
Maya Apotheker <mapothek@ucsc.edu>
2019-10-24 20:27:19 -0700

    module

--- hw3/trapezoidIntegral.f90
+++ hw3/trapezoidIntegral.f90
@@ -1,46 +1,14 @@
 
+
 program trapezoidIntegral
+use trapezoidApprox, only: trapezoidFunc,f
         implicit none
         real:: n,test
-        real(kind=8) :: trapezoidFunc
-        real(kind=8) :: f,x
+!        real(kind=8) :: trapezoidFunc
+ !       real(kind=8) :: f,x
         print *, 'hi'
         test= trapezoidFunc(25d0,0d0,1d0)
         print *, test
 end program trapezoidIntegral
-!!!
-function trapezoidFunc(n,a,b) result(aggregate)
-        real(kind=8) :: f,x
-        real(kind=8), intent(in):: n
-        real(kind=8), intent(in):: a
-        real(kind=8), intent(in):: b
-        real(kind=8):: deltaX
-        real(kind=8):: firstLast
-        real(kind=8):: middle
-        real(kind=8):: aggregate
-        print *, 'hi2'
-        deltaX=(1)/(n)
-        firstLast= deltaX*(f(a)+f(b))
-        do i=2, 24
-                print *, i*deltaX
-                middle=middle+(f(i*deltaX)*deltaX)
-!                print *, middle
-        end do
-
-                aggregate=firstLast+middle
-
-!        print *, "your approximation is"
-!        print *, aggregate
-
-
-  !      trapezoidFunc = aggregate
-
-end function trapezoidFunc
-
-real(kind=8) function f(x)
-real(kind=8), intent(in):: x
-f=x**2+1
-end function
-
 
              
---
+++ hw3/trapezoid_mod.f90
@@ -0,0 +1,54 @@
+
+module  trapezoidApprox
+implicit none
+real(kind=8), parameter:: n = 25
+!real(kind=8) ::trapezoidFunc
+!real(kind=8) ::f
+
+contains
+
+
+
+
+function trapezoidFunc(n,a,b) result(aggregate)
+        integer(kind=8) :: i
+        real(kind=8) :: x
+        real(kind=8), intent(in):: n
+        real(kind=8), intent(in):: a
+        real(kind=8), intent(in):: b
+        real(kind=8):: deltaX
+        real(kind=8):: firstLast
+        real(kind=8):: middle
+        real(kind=8):: aggregate
+        print *, 'hi2'
+        deltaX=(1)/(n)
+        firstLast= deltaX*(f(a)+f(b))
+        do i=2, 24
+                print *, i*deltaX
+                middle=middle+(f(i*deltaX)*deltaX)
+!                print *, middle
+        end do
+
+                aggregate=firstLast+middle
+
+!        print *, "your approximation is"
+!        print *, aggregate
+
+
+  !      trapezoidFunc = aggregate
+
+end function trapezoidFunc
+
+
+real(kind=8) function f(x)
+real(kind=8), intent(in):: x
+f=x**2+1
+end function
+
+
+end module trapezoidApprox
+
+
+
+
+
