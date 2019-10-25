bdf78d6c6e11dfb9fdef7e85fc835eab24651c9a
Maya Apotheker <mapothek@ucsc.edu>
2019-10-25 13:48:16 -0700

    smth

--- hw3/trapezoidIntegral.f90
+++ hw3/trapezoidIntegral.f90
@@ -6,11 +6,21 @@ use trapezoidApprox, only: trapezoidFunc,f,sub1, trapezoidExact
         real:: n,test, test2,test3
 !        real(kind=8) :: trapezoidFunc
  !       real(kind=8) :: f,x
-        print *, 'hi'
+ !       print *, 'hi'
         test= trapezoidFunc(25d0,0d0,1d0)
+		print *, 'this is test:'
 		print *, test
 		call sub1(25d0,0d0,1d0, test2)
          call trapezoidExact(0d0,1d0,test3)
+		 
+		 print*, "[1] Trapezoidal in function   =", trapezoidFunc(25d0,0d0,1d0)
+    print*, "[2] Trapezoidal in subroutine ="
+	call sub1(25d0,0d0,1d0, test2)
+    print*, "[3] Exact integration         ="
+	call trapezoidExact(0d0,1d0,test3)
+ !   print*, "[4] Error in function         =", trapezoidExact - trapezoidFunc result
+  !  print*, "[5] Error in subroutine       =", trapezoidExact - trapezoidSub result
+
         
 end program trapezoidIntegral
 
--- hw3/trapezoid_mod.f90
+++ hw3/trapezoid_mod.f90
@@ -20,16 +20,18 @@ function trapezoidFunc(n,a,b) result(aggregate)
         real(kind=8):: firstLast
         real(kind=8):: middle
         real(kind=8):: aggregate
-        print *, 'hi2'
+ !       print *, 'hi2'
         deltaX=(1)/(n)
         firstLast= deltaX*(f(a)+f(b))
         do i=2, n-1
-                print *, i*deltaX
+          !      print *, i*deltaX
                 middle=middle+(f(i*deltaX)*deltaX)
 !                print *, middle
         end do
+				aggregate=firstLast+middle
+!				print*, "[1] Trapezoidal in function   =", aggregate
 
-                aggregate=firstLast+middle
+                aggregate=aggregate+0
 
 !        print *, "your approximation is"
 !        print *, aggregate
@@ -51,25 +53,28 @@ subroutine sub1(n,a,b, test2)
         real(kind=8):: aggregate
 		real(kind=8):: test2
 		test2=0
-        print *, 'hi2'
+ !       print *, 'hi2'
         deltaX=(1)/(n)
         firstLast1= deltaX*(f(a)+f(b))
         do i=2, n-1
-                print *, i*deltaX
+         !       print *, i*deltaX
                 middle1=middle1+(f(i*deltaX)*deltaX)
 !                print *, middle
         end do
 
                 test2=firstLast1+middle1
-				print *, 'subroutine returns'
-				print *, test2
+!				print *, 'subroutine returns'
+!				print *, test2
+				
+!				print*, "[2] Trapezoidal in subroutine =", test2
 end subroutine
 
 subroutine trapezoidExact(a,b,test3)
 real(kind=8):: a,b,test3
 test3=g(b)-g(a)
-print *, 'exact returns'
-print *, test3
+!print *, 'exact returns'
+!print *, test3
+!print*, "[3] Exact integration         =", test3
 end subroutine
 
 real(kind=8) function g(x)
