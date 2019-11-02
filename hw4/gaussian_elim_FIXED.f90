9f4a8226aba179baa7c2c09f568693844d6ac8ee
unknown <crabiyan@ucsc.edu>
2019-11-02 16:53:06 -0700

    fixed backsubstitution subroutine typo, now it compiles

--- hw4/gaussian_elim_FIXED.f90
+++ hw4/gaussian_elim_FIXED.f90
@@ -18,78 +18,82 @@ module gelim
 		end subroutine gaussian_elimination
 end module gelim
 
-subroutine backsubstution(A,b,i,j)
-  real, allocatable, dimension(:,:), intent(INOUT) :: A(:,:)
-  real, allocatable, dimension(:), intent(INOUT) :: b(:)
-  real :: i,j
+module bsub
+	contains
+		subroutine backsubstitution(A,b,i,j)
+		  real, allocatable, dimension(:,:), intent(INOUT) :: A(:,:)
+		  real, allocatable, dimension(:), intent(INOUT) :: b(:)
+		  real :: i,j
 
-  ! doing back substitution
-  do j = 3, 2, -1            ! j is column
-     do i = j-1, 1, -1        ! i is row
-        factor = A(i,j)/A(j,j)
-        A(i,:) = A(i,:) - factor*A(j,:)
-        b(i) = b(i) - factor*b(j)
-     end do
-  end do
+		  ! doing back substitution
+		  do j = 3, 2, -1            ! j is column
+			 do i = j-1, 1, -1        ! i is row
+				factor = A(i,j)/A(j,j)
+				A(i,:) = A(i,:) - factor*A(j,:)
+				b(i) = b(i) - factor*b(j)
+			 end do
+		  end do
 
-  ! overwrite the solution vector to b
-  do i = 1, 10
-     b(i) = b(i)/A(i,i)
-  end do
-end subroutine backsubstution
-
+		  ! overwrite the solution vector to b
+		  do i = 1, 10
+			 b(i) = b(i)/A(i,i)
+		  end do
+		end subroutine backsubstitution
+end module bsub
 
 
 program gauss
-implicit none
-  real, allocatable, dimension(:,:) :: A(:,:)
-  real, allocatable, dimension(:) :: b(:)
-  real :: i, j
+	use gelim
+	use bsub
+	implicit none
+	real, allocatable, dimension(:,:) :: A(:,:)
+	real, allocatable, dimension(:) :: b(:)
+	real :: i, j
 
-  ! initialize matrix A and vector b
-  A(:,:) = reshape((/2, 3, -1, 4, 7, 1, 7, 10, -4/), (/3,3/))
-  b = (/1, 3, 4/)
+	! initialize matrix A and vector b
+	A(:,:) = reshape((/2, 3, -1, 4, 7, 1, 7, 10, -4/), (/3,3/))
+	b = (/1, 3, 4/)
 
 #ifdef PRINTINFO
-  ! print augmented matrix
-  do i = 1, 10           ! i is row
-     print*, A(i,:), "|", b(i)
-  end do
+		! print augmented matrix
+		do i = 1, 10           ! i is row
+			print*, A(i,:), "|", b(i)
+		end do
 #endif
 
 
 #ifdef PRINTINFO  
-  print*, ""    ! print a blank line
-  print*, "Gaussian elimination........"
+		print*, ""    ! print a blank line
+		print*, "Gaussian elimination........"
 #endif
-  call gaussian_elimination(A,b,i,j)
+		call gaussian_elimination(A,b,i,j)
 
 #ifdef PRINTINFO  
-  ! print echelon form
-  print*, "***********************"
-  do i = 1, 10
-     print*, A(i,:), "|", b(i)
-  end do
+		! print echelon form
+		print*, "***********************"
+		do i = 1, 10
+			print*, A(i,:), "|", b(i)
+		end do
 
-  print*, ""    ! print a blank line
-  print*, "back subs......"
+		print*, ""    ! print a blank line
+		print*, "back subs......"
 #endif
-  call backsubstitution(A,b,i,j)
+		call backsubstitution(A,b,i,j)
 
 
 
 #ifdef PRINTINFO  
-  ! print the results
-  print*, "***********************"
-  do i = 1, 10
-     print*, A(i,:), "|", b(i)
-  end do
+	  ! print the results
+		print*, "***********************"
+		do i = 1, 10
+			print*, A(i,:), "|", b(i)
+		end do
 
-  print*, ""
-  print*, "The solution vector is;"
-  do i = 1, 10
-     print*, b(i)
-  end do
+		print*, ""
+		print*, "The solution vector is;"
+		do i = 1, 10
+			print*, b(i)
+		end do
 #endif
 
 end program gauss
