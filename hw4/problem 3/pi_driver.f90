1896a07caf2e2451aae8de37df5b8a08b4754b3f
crabiyan <crabiyan@ucsc.edu>
2019-11-04 15:16:21 -0800

    need to use double precision flags when compiling for pi_true value to show correctly

--- hw4/problem 3/pi_driver.f90
+++ hw4/problem 3/pi_driver.f90
@@ -2,29 +2,32 @@
 
 program pi_driver
 
-     real(kind=8) :: diff, threshold, pi_true, pi_apprx, counter
-     real(kind=8) :: tempA,tempB,tempC,tempD
-     pi_true = acos(-1.d0)
-     threshold = 1.e-8
-     pi_apprx = 0.d0
-     n=0        
-     diff=0
+     use setup_module
+         call setup_init()     
 
-     do
-          tempA = 4.d0 / ((8.d0*n) + 1.d0)
-          tempB = 2.d0 / ((8.d0*n) + 4.d0)
-          tempC = 1.d0 / ((8.d0*n) + 5.d0)
-          tempD = 1.d0 / ((8.d0*n) + 6.d0)
-          pi_apprx = pi_apprx + ((16.d0**(-n)) * (tempA - tempB - tempC - tempD))
+
+     
+     do while(counter<50)
+          tempA = 4.d0 / ((8.d0*counter) + 1.d0)
+          tempB = 2.d0 / ((8.d0*counter) + 4.d0)
+          tempC = 1.d0 / ((8.d0*counter) + 5.d0)
+          tempD = 1.d0 / ((8.d0*counter) + 6.d0)
+          pi_apprx = pi_apprx + ((16.d0**(-counter)) * (tempA - tempB - tempC - tempD))
 
           diff= abs(pi_apprx - pi_true)
 
           if(diff <= threshold) EXIT
-          n=n+1
+          counter=counter+1
+          !print*, diff
+          !print*, threshold
+          !print*, counter
+          !print*, pi_apprx
+          !print*, pi_true
+          
      end do
      
      print *, "pi_true = ", pi_true
      print *, "pi_approx = ", pi_apprx
-     print *, "n = ", n
+     print *, "n = ", counter
 
 end program pi_driver
---
+++ hw4/problem 3/setup_module.F90
@@ -0,0 +1,41 @@
+!!------------------------------------------------------------------
+!! A Fortran example code for finding a root of a user-defined 
+!! function f(x) = 0.
+!! 
+!! This code is written by Prof. Dongwook Lee for AMS 209.
+!!
+!! This module has one subroutine which initialize your setup
+!! by reading in runtime parameters from 'rootFinder.init' file.
+!! The setup_init subroutine calls read_initFile*** subroutines
+!! that are implemented as subroutines in read_initFile_module. 
+!!
+!!------------------------------------------------------------------
+
+
+module setup_module
+  
+  !! include a C-type header file:
+  !! this is why the file extensions are .F90, instead of .f90
+  
+ use read_initFile_module
+ implicit none
+  real(kind=8), save :: pi_true, pi_apprx, threshold, diff
+  real, save :: tempA,tempB,tempC,tempD
+  integer, save :: counter = 0
+
+  
+contains
+
+  subroutine setup_init()
+
+    implicit none
+
+     call read_initFileReal('pi_apprx.init', 'pi_true', pi_true)
+     call read_initFileReal('pi_apprx.init', 'pi_apprx', pi_apprx)
+     call read_initFileReal('pi_apprx.init', 'threshold', threshold)
+     call read_initFileReal('pi_apprx.init', 'diff', diff)
+    
+  end subroutine setup_init
+
+  
+end module setup_module
