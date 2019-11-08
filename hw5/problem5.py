67c5994a9570dc8ea3fc8e5292a29f6381dbb0af
crabiyan <crabiyan@ucsc.edu>
2019-11-08 13:29:25 -0800

    problem 5 finished

--- hw5/problem5.py
+++ hw5/problem5.py
@@ -4,16 +4,20 @@ l = ["cameron", "rabiyan"]
 def print_yourName(l, param=None):
 	if param is not None:
 		if param == False:
-			for i in reversed(l):
+			for i in reversed(range(len(l))):
 				if i == 0:
 					print (l[i].capitalize())
 				else:
 					print (l[i].capitalize(), end = ", ")
 		else:
-			print ("true")
+			for i, value in enumerate(l):
+				print (l[i].capitalize(), end = " ")
+			print ("")
 	else:
 		for i, value in enumerate(l):
 			print (l[i].capitalize(), end = " ")
 		print ("")
 		
 print_yourName(l)
+print_yourName(l, True)
+print_yourName(l, False)
