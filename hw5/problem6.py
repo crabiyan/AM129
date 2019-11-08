5a70c5c77255eb93646be44b04f846650234629f
crabiyan <crabiyan@ucsc.edu>
2019-11-08 13:55:00 -0800

    problem 6 finished

---
+++ hw5/problem6.py
@@ -0,0 +1,21 @@
+import copy
+
+
+def verify_palindrome(s, i, count):
+	if i >= 2:
+		if s[i] == s[count]:
+			i=i-1
+			count=count+1
+			verify_palindrome(s, i, count)
+		else:
+			print ("False")
+			return False
+	else:
+		print ("True")
+		return True
+
+count = 0
+word = input("Please enter a palindrome(Case-Sensitive): ")
+
+verify_palindrome(word, len(word)-1, count)
+
