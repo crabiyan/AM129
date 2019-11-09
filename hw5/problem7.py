dd7cf26e2572923aa56a21f95a27ea5879021a95
crabiyan <crabiyan@ucsc.edu>
2019-11-08 20:12:11 -0800

    Problem 7 finished

--- hw5/problem7.py
+++ hw5/problem7.py
@@ -1,21 +1,28 @@
-import copy
-
-
 def get_realnum(n):
 	a = []
 	i = 0
 	while i < n:
 		l = [0,0]
 		print("Coordinate %d: " % i)
-		l[0]=input("Please enter the X coordinate: ")
-		l[1]=input("Please enter the Y coordniate: ")
+		l[0]=float(input("Please enter the X coordinate: "))
+		l[1]=float(input("Please enter the Y coordniate: "))
 		a.append(l)
 		print (l)
 		i += 1
-
-
+	return a
+def is_triangle(points):
+	num1 = points[0]
+	num2 = points[1]
+	num3 = points[2]
+	if num1[0] == num2[0] == num3[0] or num1[1] == num2[1] == num3[1]:
+		print ("Not a triangle")
+		return False
+	else:
+		print ("Is a triangle")
+		return True
+	
 total_numbers = 3
-get_realnum(total_numbers)
+is_triangle(get_realnum(total_numbers))
 
 
 
