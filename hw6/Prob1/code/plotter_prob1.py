a673d75324981fa10a206b96cc6f0977e62440bd
crabiyan <crabiyan@ucsc.edu>
2019-11-18 15:38:22 -0800

    problem 1 completed

--- hw6/Prob1/code/plotter_prob1.py
+++ hw6/Prob1/code/plotter_prob1.py
@@ -1,17 +1,23 @@
+####################################################
+# AM129 hw6
+#
+# Students:
+# Cameron Rabiyan - Driver
+# Maya Apotheker - Navigator
+# Manny Gamboa - Navigator
+#
+# Group 334-7
+####################################################
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def word_count(file):
-	#f.open(file, 'r')
 	d = dict()
 	with open(file, 'r') as f:
 		while True:
 			c = f.read(1)			
 			if not c:
-				print ("-----------")
-				print ("End of file")
-				print ("-----------")
 				break
 			elif c != "\n":
 				if c in d.keys():
@@ -20,12 +26,31 @@ def word_count(file):
 					d.update({c:val})
 				else:
 					d[c] = 1
-					#print ("character: ", c)
-		for key, value in d.items():
-			print (key, value)
-
 	return d
+
 if __name__ == "__main__":
+	l = list()
+	arr = np.array([], dtype=int)
+	
 	file_path = './words.txt'
 	hist = word_count(file_path)
 	
+	x = np.arange( len(hist.keys()) )
+	y = np.arange( len(hist.values()) )
+	nCounts = []
+	plt.xlabel('Character')
+	plt.ylabel('Frequency')
+	
+	for key in sorted(hist.keys()):
+		l.append(key)
+		arr = np.append(arr, [hist[key]])
+		print ("'"+key+"'"+":",hist[key])
+		plt.bar(key, hist[key], color='blue')
+	
+	plt.plot(x,arr,marker='o', color='r')
+	plt.xticks(x, l)
+	plt.yticks(np.arange(0,120000, 20000))
+	plt.grid(b=None, which = 'major', axis='both')
+	plt.savefig('results_prob1.png')
+	plt.show()
+
